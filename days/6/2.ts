ca75d229f42545bbb5757eca8ccfa41a594722cd
nonAlgebraic <12376495+nonAlgebraic@users.noreply.github.com>
2022-12-06 10:56:42 -0500

    day 6 part 2

---
+++ days/6/2.ts
@@ -0,0 +1,21 @@
+const SIGNAL_LENGTH = 14;
+
+export default (input: string) => {
+	let candidateI = 0;
+
+	for (let i = 1; i < input.length; i++) {
+		let uniqueCount = 0;
+
+		for (let j = candidateI; j < i; j++) {
+			if (input[i] !== input[j]) {
+				uniqueCount++;
+			} else {
+				candidateI = j;
+			}
+		}
+
+		if (uniqueCount === SIGNAL_LENGTH) {
+			return i + 1;
+		}
+	}
+};
